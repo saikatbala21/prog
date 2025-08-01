c056f0460792a59696fcdf2a83ec88b09f68e372
saikatbala21 <saikatbala21.sm@gmail.com>
2025-08-01 15:28:28 +0530

    Add files via upload

---
+++ add no.py
@@ -0,0 +1,6 @@
+n=50
+sum=0
+for i in range(1,n):
+    sum=sum+i
+    a=sum
+    print(a, end=" ")
---
+++ main.py
@@ -0,0 +1,6 @@
+n=int(input("enter the numer:"))
+if (n%2==0):
+    print(n,"the number is even")
+else:
+    print(n,"the number is odd")
+    print("Thank you")
